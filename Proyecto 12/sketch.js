2d5f1c2ad00f94eeb6b468331e6709e81e4eddea
Jesús Ángel <84876922+ChuyGS12@users.noreply.github.com>
2022-01-21 21:44:43 -0600

    Add files via upload

---
+++ Proyecto 12/sketch.js
@@ -0,0 +1,43 @@
+var path,boy, leftBoundary,rightBoundary;
+var pathImg,boyImg;
+var i;
+
+function preload(){
+  pathImg=loadImage ("path.png");
+ boyImg=loadAnimation("jake1.png","jake2.png","jake3.png","jake4,png","jake5.png");}
+
+function setup(){
+  
+  createCanvas(400,400);
+  path=createSprite(200,200);
+  path.addImage("path",pathImg);
+  path.velocityY=10;
+  path.scale=1.2;
+
+boy=createSprite(200,200);
+boy.addAnimation("running",boyImg);
+boy.scale=0.08;
+  
+leftBoundary=createSprite(0,0,100,800);
+leftBoundary.visible=false;
+
+rightBoundary=createSprite(410,0,100,800);
+rightBoundary.visible=false;}
+
+function draw() {
+  background(0);
+  path.velocityY = 4;
+  
+  boy.x=World.mouseX;
+  
+  edges= createEdgeSprites();
+  boy.collide(edges[3]);
+  boy.collide(leftBoundary);
+  boy.collide(rightBoundary);
+
+  if(path.y > 400 ){
+    path.y = height/2;
+  }
+  
+  drawSprites();
+}
